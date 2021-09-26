c47d1f1c6410bc06a4ffdfe4078a200bb11ced0f
aryawidiap <aryawidiaputra2002@gmail.com>
2021-09-26 12:05:53 +0800

    add components

---
+++ src/component/Featured.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+const Featured = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setData(data.meals);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {error ? <h2>Error fetching data</h2> : null}
+      {data ? (
+        <>
+          <img src={data[0].strMealThumb} alt={data[0].strMeal} />
+          <p><em>featured</em></p>
+          <h2>{data[0].strMeal}</h2>
+          <Link to={"/details:" + data[0].idMeal}>
+            <em>Details &gt;</em>{" "}
+          </Link>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default Featured;
